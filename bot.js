cfc00e666c4357aa89e126c3b99a4c99ec467823
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-11-07 13:59:31 -0500

    Make some large changes.

--- bot.js
+++ bot.js
@@ -9,14 +9,24 @@ const bot = new irc.Client(settings.network, settings.name, {
     realName = settings.realName,
     channels = [settings.channels.chan0.chan]
 });
+bot.on('registered', () =>{
+    console.log(`Logged in to $(settings.network) as $(settings.name)\nReal name is: $(settings.realname)`)
+})
 
 // Join Handler
 bot.on('join', (channel, name, msg) => {
     if (name === settings.name) {break}; // If self, then quit.
-    if (settings.botops.indexOf(name) > -1) {
-        bot.notice(name, `Please /msg $(settings.name) with your password.`)
-    };
-    if (settings.channel)
+    if (users[users.indexOf(name)].botop != false) {
+        if (users[user.indexOf(name)].botop == true) {
+            bot.notice(name, `You have not set a bot operator password.  This is hightly insecure.`)
+            bot.notice(name, `Please '/msg $(settings.name) botop register' with an operator password.  Until then, you will not be able to control the bot.`)
+        } else {
+            bot.notice(name, `Please '/msg $(settings.name) botop signin' with your operator password to gain use of the operator commands.`)
+        }
+    }
+    if (users[users.indexOf(name)].chanop[users[users.indexOf(name)].chanop.indexOf(channel)] != false) {
+        
+    }
 });
 
 // Command Handler
