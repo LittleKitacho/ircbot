c4fed31d190aa9b65c1f890fd2ea4ad189349572
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-11-07 13:31:13 -0500

    Make some changes

--- bot.js
+++ bot.js
@@ -2,17 +2,24 @@
 const fs = require('fs');
 let rawdata = fs.readFileSync("./settings.json");
 let settings = json.parse(rawdata);
-const saveSettings = function() {
-    rawdata = json.stringify(settings, null, 2);
-    fs.writeFileSync(rawdata);
-};
 
 // Setup IRC.
 const irc = require('node-irc');
-const bot = new irc.Client(settings.network.hyperlink, settings.name, {
+const bot = new irc.Client(settings.network, settings.name, {
     realName = settings.realName,
     channels = [settings.channels.chan0.chan]
-})
+});
 
-// PM Handler
-bot.addListener("PM")
+// Join Handler
+bot.on('join', (channel, name, msg) => {
+    if (name === settings.name) {break}; // If self, then quit.
+    if (settings.botops.indexOf(name) > -1) {
+        bot.notice(name, `Please /msg $(settings.name) with your password.`)
+    };
+    if (settings.channel)
+});
+
+// Command Handler
+bot.on('message', (channel, name, msg) => {
+
+});
