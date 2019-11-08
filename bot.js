032b6a28bdb00ce5aba23274b5fc2c1f9c0390cd
LittleKitacho <dbudke1010@outlook.com>
2019-11-08 06:43:17 -0500

    Make some large changes.

--- bot.js
+++ bot.js
@@ -1,34 +1,32 @@
 // Setup Settings
 const fs = require('fs');
-var rawdata = fs.readFileSync("./settings.json");
-var settings = json.parse(rawdata);
-var rawdata = fs.readFileSync("./users.json");
-var userdata = json.parse(rawdata);
+const databases = "./databases/";
+// Get raw data
+let settingsraw = fs.readFileSync(databases+"settings.json");
+let usersraw = fs.readFileSync(databases+"users.json");
+let channelsraw = fs.readFileSync(databases+"channels.json");
+// Parse raw data
+let settings = JSON.parse(settingsraw);
+let users = JSON.parse(usersraw);
+let channels = JSON.parse(channelsraw);
 
 // Setup IRC.
-const irc = require('node-irc');
-const bot = new irc.Client(settings.network, settings.name, {
+const irc = require('irc');
+const bot = new irc.Client(settings.network, settings.name, [
     realName = settings.realName,
-    channels = [settings.channels.chan0.chan]
-});
+    channels = [settings.channels]
+]);
 bot.on('registered', () =>{
-    console.log(`Logged in to $(settings.network) as $(settings.name)\nReal name is: $(settings.realname)`)
+    console.log(`Logged in as $(Client.nick)`)
+    bot._clearWhoisData();
+    bot._addWhoisData(settings.whois)
 })
 
 // Join Handler
 bot.on('join', (channel, name, msg) => {
-    if (name === settings.name) {break}; // If self, then quit.
-    if (users[users.indexOf(name)].botop != false) {
-        if (users[user.indexOf(name)].botop == true) {
-            bot.notice(name, `You have not set a bot operator password.  This is hightly insecure.`)
-            bot.notice(name, `Please '/msg $(settings.name) botop register' with an operator password.  Until then, you will not be able to control the bot.`)
-        } else {
-            bot.notice(name, `Please '/msg $(settings.name) botop signin' with your operator password to gain use of the operator commands.`)
-        }
-    }
-    if (users[users.indexOf(name)].chanop[users[users.indexOf(name)].chanop.indexOf(channel)] != false) {
-        
-    }
+    var uid = users.indexOf(name);
+    var cid = channels.indexOf(channel);
+    if (users[uid].botop == true) {};
 });
 
 // Command Handler
